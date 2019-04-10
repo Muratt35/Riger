68a56d455f85ddd6253c9876ed696e0e7489d5ea
Muratt35 <44320725+Muratt35@users.noreply.github.com>
2019-04-11 00:45:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
     
+client.on('message', message => {
+    if (message.content === 'sil')
+  let mesajsayisi = parseInt(args.join(' '));
+  if (mesajsayisi.length < 1) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
+  if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
+  message.channel.bulkDelete(mesajsayisi + 1);
+  message.channel.send(mesajsayisi +' adet mesaj sildim!')
+};
 
 client.on('message', message => {
     if (message.content === 'Rbd') {
